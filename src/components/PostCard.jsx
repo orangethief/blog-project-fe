813c58b3b0a8211e199f17f0968baa7a4cf1d97c
Lisa <helbig-lisa@web.de>
2024-10-27 01:15:18 +0200

    functionalities

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -6,21 +6,25 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { Badge } from '@/components/ui/Badge';
 
 
 const PostCard = ({ post }) => {
-    const previewText = post.content.length > 50 ? post.content.slice(0, 50) + '...' : post.content;
+    const previewText = post.content.length > 50 ? post.content.slice(0, 80) + '...' : post.content;
 
     return (
         <Link to={`/post/${post.id}`} className="cursor-pointer transition-transform transform hover:scale-105 duration-500 ease-in-out">
           <Card>
             <CardHeader>
-              <CardTitle className="text-xl">{post.title}</CardTitle>
-              <CardDescription className="text-sm">by {post.author}</CardDescription>
+              <CardTitle className="text-xl min-h-[3.5rem] ">{post.title}</CardTitle>
+              <CardDescription className="text-sm">
+                <Badge  className="rounded-md">{post.author}
+                </Badge>
+              </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-4">
               {post.cover && <img src={post.cover} alt={post.title} className="w-96 h-48 object-cover" />}
-              <CardDescription>{previewText}</CardDescription>
+              <CardDescription className="line-clamp-2">{post.content}</CardDescription>
             </CardContent>
           </Card>
         </Link>
--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,19 +1,22 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { fetchPostById } from '@/utils/api';
+import { fetchPostById, deletePost } from '@/utils/api';
 import { getFirstSentence, formatDate } from '@/utils/utils';
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { c } from 'vite/dist/node/types.d-aGj9QkWt';
+import { Button } from '@/components/ui/Button';
+import { SquareArrowLeft } from 'lucide-react';
+
 
 const PostDetails = () => {
   const { id } = useParams();
-  console.log(id)
+  const navigate = useNavigate();
   const [post, setPost] = useState(null);
 
   useEffect(() => {
@@ -24,15 +27,26 @@ const PostDetails = () => {
     loadPost();
   }, [id]);
 
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this post?');
+    if (confirmDelete) {
+      await deletePost(id);
+      navigate('/');
+    }
+  };
+
   if (!post) return <p>Loading...</p>;
 
   const firstSentence = getFirstSentence(post.content);
   const formattedDate = formatDate(post.date);
 
   return (
-    <div className="min-h-screen p-6 bg-background dark text-foreground">
+    <div className="min-h-screen py-6 bg-background dark text-foreground">
 
           <Card className="border-none">
+            <Link to="/" className="cursor-pointer">
+              <SquareArrowLeft className="text-primary h-12 w-12 ml-6 hover:text-primary/90" />
+            </Link>
             <CardHeader className="p-8">
               <CardTitle className="text-4xl text-center font-extrabold uppercase tracking-wider mb-1">{post.title}</CardTitle>
               <CardDescription className="text-sm text-center">{firstSentence}</CardDescription>
@@ -41,10 +55,18 @@ const PostDetails = () => {
               <div className="max-h-[25vh] lg:max-h-[50vh] overflow-hidden">
                 {post.cover && <img src={post.cover} alt={post.title} className="w-full max-h-[25vh] lg:max-h-[50vh] object-cover" />}
               </div>
-              <CardDescription className="italic text-bold">written by {post.author}</CardDescription>
-              <CardDescription className="italic text-bold">Published on {formattedDate}</CardDescription>
+
+              <CardDescription className="text-bold ">
+                <div className="italic pb-1">written by {post.author}</div>
+                <div>Published on {formattedDate}</div>
+              </CardDescription>
+              <CardDescription className="italic text-bold"></CardDescription>
               <CardDescription>{post.content}</CardDescription>
             </CardContent>
+            <CardFooter className="flex justify-center space-x-4">
+              <Button>Edit Post</Button>
+              <Button variant="destructive" onClick={handleDelete}>Delete Post</Button>
+            </CardFooter>
           </Card>
     </div>
   )
